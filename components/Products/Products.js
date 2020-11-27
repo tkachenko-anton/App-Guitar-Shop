8307e82c27e2ef101c9c6cd71b2e21f195f4d657
Anton Tkachenko <tkachenko.da.bucha@gmail.com>
2020-11-27 21:34:43 +0200

    Products.js and root.js were changed

---
+++ components/Products/Products.js
@@ -0,0 +1,30 @@
+class Products {
+
+
+  render() {
+    let htmlCatalog = '';
+
+    CATALOG.forEach(({ id, name, price, img }) => {
+      
+      htmlCatalog += `
+      <li>
+        <span>${name}</span>
+        <img src="${img}" />
+        <span>${price}</span>
+        <button>Add to Cart</button>
+      </li>
+      `;
+    });
+
+    const html = `
+      <ul>
+        ${htmlCatalog}
+      </ul>
+    `;
+
+    ROOT_PRODUCTS.innerHTML = html;
+  }
+}
+
+const productsPage = new Products();
+productsPage.render();
