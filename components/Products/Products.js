b3379c0f89575152022ded5a3335c229c9a9c425
Anton Tkachenko <tkachenko.da.bucha@gmail.com>
2020-11-30 23:58:25 +0200

    Products.css and index.css was changed

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -7,17 +7,17 @@ class Products {
     CATALOG.forEach(({ id, name, price, img }) => {
       
       htmlCatalog += `
-      <li>
-        <span>${name}</span>
-        <img src="${img}" />
-        <span>${price}</span>
-        <button>Add to Cart</button>
+      <li class="products-element">
+        <span class="products-element__name">${name}</span>
+        <img class="products-element__img" src="${img}" />
+        <span class="products-element__price">⚡️ ${price.toLocaleString()} BTC </span>
+        <button class="products-element__btn">Add to Cart</button>
       </li>
       `;
     });
 
     const html = `
-      <ul>
+      <ul class="products-container" >
         ${htmlCatalog}
       </ul>
     `;
