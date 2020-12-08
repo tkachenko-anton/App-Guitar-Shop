8b89670d4788395a54e096f9a65b5f169488e787
Anton Tkachenko <tkachenko.da.bucha@gmail.com>
2020-12-08 13:16:33 +0200

    catalog.json was added

---
+++ index.js
@@ -0,0 +1,19 @@
+function render() {
+  const productsStore = localStorageUtil.getProducts();
+  
+  headerPage.render(productsStore.length);
+  productsPage.render();
+}
+
+let CATALOG = [];
+
+// https://api.myjson.com/bins/esicc
+fetch('server/catalog.json')
+  .then(res => res.json())
+  .then(body => {
+    CATALOG = body;
+    render();
+  })
+  .catch(error => {
+    console.log(error);
+  });
